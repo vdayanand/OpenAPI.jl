f9bcb17db1c8e9c72a70ca818ea7159cf2d701f2
vdayanand <venkatesh@juliacomputing.com>
2024-07-09 18:14:24 +0530

    fix

--- test/deep_object/deep_client.jl
+++ test/deep_object/deep_client.jl
@@ -1,64 +1,21 @@
-module FormsV3Client
+module DeepClientTest
 
-include("FormsClient/src/FormsClient.jl")
-using .FormsClient
+include("DeepClient/src/DeepClient.jl")
+using .DeepClient
+using .DeepClient.OpenAPI.Clients: Client
+using .DeepClient: FindPetsByStatusStatusParameter
+
 using Test
-using OpenAPI
-using OpenAPI.Clients
-import OpenAPI.Clients: Client
-using Base64
 
 const server = "http://127.0.0.1:8081"
 
-function test(uri)
-    @info("FormsClient.DefaultApi")
-    client = Client(uri)
-    api = FormsClient.DefaultApi(client)
-
-    mktemp() do test_file_path, test_file_io
-        file_contents = "file contents"
-        print(test_file_io, file_contents)
-        close(test_file_io)
-
-        api_return, http_resp = FormsClient.post_urlencoded_form(api, 1, file_contents; additional_metadata="my metadata")
-        @test isa(api_return, FormsClient.TestResponse)
-        @test api_return.message == "success, form_id=1, metadata=my metadata, file=file contents"
-        @test http_resp.status == 200
-
-        api_return, http_resp = FormsClient.upload_binary_file(api, 1; additional_metadata="my metadata", file=test_file_path)
-        @test isa(api_return, FormsClient.TestResponse)
-        @test api_return.message == "success, file_id=1, metadata=my metadata, file=file contents"
-        @test http_resp.status == 200
-
-        api_return, http_resp = FormsClient.upload_text_file(api, 1; additional_metadata="my metadata", file=Base64.base64encode(file_contents))
-        @test isa(api_return, FormsClient.TestResponse)
-        @test api_return.message == "success, file_id=1, metadata=my metadata, file=file contents"
-        @test http_resp.status == 200
-    end
-
-    return nothing
-end
-
 function runtests()
-    @testset "Forms and File Uploads" begin
-        test(server)
-    end
-end
-
-end # module FormsV3Client
-
-module DeepObjectClientTest
-include("deep_client/src/deep.jl")
-using .deep
-using .deep.OpenAPI.Clients: Client
-using .deep: FindPetsByStatusStatusParameter
-
-function test()
-    client = Client("localhost:8081")
-    api = deep.PetApi(client)
+    @info("PetApi")
+    client = Client(server)
+    api = DeepClient.PetApi(client)
     unsold = FindPetsByStatusStatusParameter("key", ["available", "pending"])
     _, http_resp = find_pets_by_status(api, unsold)
-    @info http_resp
+    @test http_resp.status == 200
 end
 
 end # module DeepObjectClientTest
